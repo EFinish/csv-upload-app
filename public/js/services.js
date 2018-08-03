aaf1f57da326de02b23c8a1b42b7e654b25017ac
EFinish <EFinish@gmail.com>
2018-08-03 12:36:21 -0500

    services designed so that they cannot be modified only used and consumed by other code)

--- public/js/services.js
+++ public/js/services.js
@@ -1,6 +1,23 @@
 //front end services for application
 
 //file service: action 1 = upload
+var FileService = (function(){
+    var upload = function(file) {
+        //upload file via post to /upload
+    };
 
+    return {
+        upload: upload
+    };
+})();
 
 //people service: action 1 = search
+var PeopleService = (function(){
+    var search = function(query) {
+        //post query using {query: query}
+    };
+
+    return {
+        search: search
+    };
+})();
