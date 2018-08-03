8f557ec4ee170c1eedb70aba1caba5f429242640
EFinish <EFinish@gmail.com>
2018-08-03 17:28:19 -0500

    webpack application WIP

--- webpackapp.js
+++ webpackapp.js
@@ -5,10 +5,20 @@ import Vue from './node_modules/vue/dist/vue.esm.browser.js';
 // import Bootstrap from './node_modules/bootstrap/dist/js/bootstrap.min.js'
 import BootstrapVue from './node_modules/bootstrap-vue/dist/bootstrap-vue.min.js';
 
+//have vue actually use vue bootstrap
 Vue.use(BootstrapVue);
 
+//import front end services
+import services from './public/js/services.js';
+
+//import vue components to be used in vue application
 import components from './public/js/index/components.js';
 
+components.use(Vue);
+components.use(services);
+
+
+//the Vue app instance itself
 var app = new Vue({
     el: '#app',
     data: {
