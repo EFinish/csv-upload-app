3c9a774e775fcd0f3750953cd1382b8b189e9ecc
EFinish <EFinish@gmail.com>
2018-08-03 16:41:09 -0500

    the webpack app file

---
+++ webpackapp.js
@@ -0,0 +1,6 @@
+//Vue Dependencies
+import Vue from './node_modules/vue/dist/vue.esm.browser.js';
+
+//Vue Bootstrap dependencies
+// import Bootstrap from './node_modules/bootstrap/dist/js/bootstrap.min.js'
+import BootstrapVue from './node_modules/bootstrap-vue/dist/bootstrap-vue.min.js';
