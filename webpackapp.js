724db9721545053f01d493858fcbd05f08111642
EFinish <EFinish@gmail.com>
2018-08-03 17:21:51 -0500

    put vue app in webpackapp

--- webpackapp.js
+++ webpackapp.js
@@ -4,3 +4,21 @@ import Vue from './node_modules/vue/dist/vue.esm.browser.js';
 //Vue Bootstrap dependencies
 // import Bootstrap from './node_modules/bootstrap/dist/js/bootstrap.min.js'
 import BootstrapVue from './node_modules/bootstrap-vue/dist/bootstrap-vue.min.js';
+
+Vue.use(BootstrapVue);
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        file_loaded: false,
+        people: []
+    },
+    methods: {
+        setFileLoaded: function(value) {
+            this.file_loaded = value;
+        },
+        setPeople: function(values) {
+            this.people = values;
+        }
+    }
+});
