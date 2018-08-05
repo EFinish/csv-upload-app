bf7de46aa44519c25246e77efc37353924f2a23c
EFinish <EFinish@gmail.com>
2018-08-05 12:02:52 -0500

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,32 @@
+// const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 module.exports = {
+    // web pack maing file
     entry: "./webpackapp.js",
+    //where to output the bundled file to
     output: {
-        path: __dirname+'/public/js/dist/',
+        path: __dirname + '/public/js/dist/',
         filename: "bundle.js"
     },
-    watch: true
-};
+    watch: true,
+
+    module: {
+        rules: [
+            // allow .vue files
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            // allow <script> in .vue files
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        // make sure to include the plugin for the magic
+        new VueLoaderPlugin()
+    ]
+}; 
