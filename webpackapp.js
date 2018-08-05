b13d76e0d025c18e375d6cd67d91867d567c0038
EFinish <EFinish@gmail.com>
2018-08-05 11:59:51 -0500

    webpack app changes

--- webpackapp.js
+++ webpackapp.js
@@ -1,26 +1,29 @@
 //Vue Dependencies
 import Vue from './node_modules/vue/dist/vue.esm.browser.js';
 
-//Vue Bootstrap dependencies
-// import Bootstrap from './node_modules/bootstrap/dist/js/bootstrap.min.js'
-import BootstrapVue from './node_modules/bootstrap-vue/dist/bootstrap-vue.min.js';
+// //Vue Bootstrap dependencies
+// // import Bootstrap from './node_modules/bootstrap/dist/js/bootstrap.min.js'
+// import BootstrapVue from './node_modules/bootstrap-vue/dist/bootstrap-vue.min.js';
 
-//have vue actually use vue bootstrap
-Vue.use(BootstrapVue);
+// //have vue actually use vue bootstrap
+// Vue.use(BootstrapVue);
 
-//import front end services
-import services from './public/js/services.js';
+// //import front end services
+// import services from './public/js/services.js';
 
-//import vue components to be used in vue application
-import components from './public/js/index/components.js';
+// //import vue components to be used in vue application
+import CsvFileInput from './public/vue/CsvFileInput.vue';
 
-components.use(Vue);
-components.use(services);
+// components.use(Vue);
+// components.use(services);
 
 
 //the Vue app instance itself
 var app = new Vue({
     el: '#app',
+    components: {
+        CsvFileInput
+    },
     data: {
         file_loaded: false,
         people: []
