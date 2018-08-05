db3238725e8d4499371322f88890d00967df5f55
EFinish <EFinish@gmail.com>
2018-08-05 12:05:28 -0500

    got rid of old components file and put into .vue file

--- public/js/index/components.js
+++
@@ -1,7 +0,0 @@
-Vue.component('csv-file-input', {
-    template:
-        '<div class="col-sm-12">' +
-        '<label for="uploadField">CSV file to upload</label>' +
-        '<input id="uploadField" type="file" class="form-control">'+
-        '</div>'
-});
---
+++ public/vue/CsvFileInput.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="col-sm-12">
+        <label for="uploadField">CSV file to upload</label>
+        <input id="uploadField" type="file" class="form-control">
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
