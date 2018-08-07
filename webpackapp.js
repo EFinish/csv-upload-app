6b81f5a5687e84c5ef9d35e117282b407c2d6733
EFinish <EFinish@gmail.com>
2018-08-06 20:36:07 -0500

    eventbus (for routing events to parent app, prevent children form modifying parent directly), added progress percentage and progress step data

--- webpackapp.js
+++ webpackapp.js
@@ -14,24 +14,65 @@ Vue.use(BootstrapVue);
 //import vue components to be used in vue application
 import UploadField from './public/vue/UploadField.vue';
 import SearchField from './public/vue/SearchField.vue';
+import LoadingBar from './public/vue/LoadingBar.vue';
+
+var EventBus = new Vue();
 
 //the Vue app instance itself
 var app = new Vue({
     el: '#app',
+    created: function() {
+        EventBus.$on('set-file-loaded', this.setFileLoaded);
+        EventBus.$on('set-uploading', this.setUploading);
+        EventBus.$on('set-progress-percentage', this.setProgressPercentage);
+        EventBus.$on('set-progress-step', this.setProgressStep);
+    },
+    destroyed: function() {
+        EventBus.$off('set-file-loaded', this.setFileLoaded);
+        EventBus.$off('set-uploading', this.setUploading);
+        EventBus.$off('set-progress-percentage', this.setProgressPercentage);
+        EventBus.$off('set-progress-step', this.setProgressStep);
+    },
     components: {
         UploadField,
-        SearchField
+        SearchField,
+        LoadingBar
     },
     data: {
-        file_loaded: false
+        file_loaded: false,
+        uploading: false,
+        progress_percentage: 0,
+        progress_step: null
+    },
+    computed: {
+        uploadable: function() {
+            return (!this.file_loaded && !this.uploading);
+        },
+        searchable: function(){
+            return (this.file_loaded && !this.uploading);
+        }
     },
     methods: {
         setFileLoaded: function(value) {
+            console.log("file loaded "+value);
             this.file_loaded = value;
+        },
+        setUploading: function(value) {
+            console.log("uploading "+value);
+            this.uploading = value;
+        },
+        setProgressPercentage: function(data) {
+            this.progress_percentage = data;
+        },
+        setProgressStep: function(data) {
+            this.progress_step = data;
         }
     }
 });
 
 //css dependencies
-import './node_modules/bootstrap/dist/css/bootstrap-grid.min.css';
+import './node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './node_modules/bootstrap-vue/dist/bootstrap-vue.min.css';
+
+export {EventBus};
 
