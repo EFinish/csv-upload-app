2e10276a6be3d4c733de0aeb443d2c04dcb4bcaa
EFinish <EFinish@gmail.com>
2018-08-06 20:37:19 -0500

    loading bar vue (using vue bootstrap loading bar!)

---
+++ public/vue/LoadingBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="row">
+        <div class="col-sm-12" align="center">
+            <p>Currently {{progress_step}}...</p>
+            <b-progress :value="progress_percentage" :max="100" :variant="variant_class" show-progress animated>
+            </b-progress>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        "progress_percentage",
+        "progress_step"
+    ],
+    calculated: {
+        variant_class: function() {
+            if(this.progress_step === "Uploading file") {
+                return "warning";
+            } else {
+                return "success";
+            }
+        }
+    }
+}
+</script>
