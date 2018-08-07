75b6c8067b13468303649ce05eceeb5287c2e12c
EFinish <EFinish@gmail.com>
2018-08-06 20:38:03 -0500

    proper staging of events for upload all via EventBus

--- public/vue/UploadField.vue
+++ public/vue/UploadField.vue
@@ -7,8 +7,13 @@
 
 <script>
 import {FileService} from '../js/services';
+
+import {EventBus} from '../../webpackapp.js';
 // var axios = require('axios');
 export default {
+    props: [
+        'uploading'
+    ],
     data: function() {
         return {
             file: null
@@ -25,22 +30,31 @@ export default {
             }
         },
         uploadFile: function() {
+            //set uploading and progress
+            EventBus.$emit('set-uploading', true);
+            EventBus.$emit('set-progress-step', 'Uploading file');
+
             //prepare data to send to service
             let formData = new FormData();
             formData.append('file', this.file);
 
+            var onUploadProgressCallback = function(uploadTotal) {
+                EventBus.$emit('set-progress-percentage', uploadTotal);
+                if (uploadTotal === 100) {
+                    EventBus.$emit('set-progress-step', 'Parsing file');
+                }
+            }
+
             //send to service
-            FileService.upload(this.file)
+            FileService.upload(this.file, onUploadProgressCallback)
             .then(function(response){
+                EventBus.$emit('set-uploading', false);
+                EventBus.$emit('set-file-loaded', true);
                 console.log("done!");
-                // console.log(response);
             })
             .catch(function(){
                 console.log('FAILURE!!');
             });
-
-            //after upload success
-            // app.setFileLoaded(true);
         }
     }
 }
